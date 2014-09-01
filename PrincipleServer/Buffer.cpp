839621e3433f54deebc2b31445801fc960cf1a62
Principle <qq357478904@gmail.com>
2014-09-01 22:54:32 +0800

    The fist .

---
+++ PrincipleServer/Buffer.cpp
@@ -0,0 +1,11 @@
+/********************************************************************
+	created:	2014/09/01
+	created:	1:9:2014   22:50
+	filename: 	H:\PrincipleServer\PrincipleServer\Buffer.cpp
+	file ext:	cpp
+	author:		Principle
+	
+	purpose:	This file achieve CBuffer class.
+*********************************************************************/
+#include "stdafx.h"
+#include "Buffer.h"
---
+++ PrincipleServer/Buffer.h
@@ -0,0 +1,16 @@
+/********************************************************************
+	created:	2014/09/01
+	created:	1:9:2014   22:44
+	filename: 	H:\PrincipleServer\PrincipleServer\Buffer.h
+	file ext:	h
+	author:		Principle
+	
+	purpose:	This file define CBuffer Class used in iocp model.
+*********************************************************************/
+#pragma once
+#include "windows.h"
+class CBuffer
+{
+private:
+	
+};
